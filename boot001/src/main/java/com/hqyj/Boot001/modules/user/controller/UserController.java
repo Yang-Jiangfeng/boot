51facc25c38baff64dd12bb13dab1d4ad82f5cca
Yang-Jiangfeng <13551490760@163.com>
2020-08-10 14:32:03 +0800

    "boot提交user"

---
+++ boot001/src/main/java/com/hqyj/Boot001/modules/user/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.hqyj.Boot001.modules.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping("/userDesc")
+    @ResponseBody
+    public String testDesc() {
+        return "This is user module desc.";
+    }
+
+}
